80960a38d5a9231a39f30c63d26e019d057146d4
3A532059 <coco214034@gmail.com>
2018-10-19 14:38:52 +0800

    建立tasks視圖&模版

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 陳姿婷
+ * Date: 2018/10/19
+ * Time: 下午 02:34
+ */
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel 快速入門 - 基本</title>
+
+        <!-- CSS 及 JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar 內容 -->
+            </nav>
+        </div>
+
+@yield('content')
+</body>
+</html>
