e5069dc965c3269c387d060ba6fb8f77dd326b30
3A532059 <coco214034@gmail.com>
2018-10-19 16:40:28 +0800

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 陳姿婷
- * Date: 2018/10/19
- * Time: 下午 02:34
- */
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Laravel 快速入門 - 基本</title>
+<head>
+    <title>Laravel 快速入門 - 基本</title>
 
-        <!-- CSS 及 JavaScript -->
-    </head>
+    <!-- CSS 及 JavaScript -->
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
 
-    <body>
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <!-- Navbar 內容 -->
-            </nav>
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
         </div>
+    </nav>
+</div>
 
 @yield('content')
+
+<!-- Scripts -->
+<script src="/js/app.js"></script>
+
 </body>
 </html>
