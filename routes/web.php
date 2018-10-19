745b43ed4b5a27057eaabb9d932dd9e328dba3fd
3A532059 <coco214034@gmail.com>
2018-10-19 15:27:27 +0800

    顯示所有任務於tasks視圖上

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,26 @@ Route::get('/', function () {
 // 增加新的任務
     Route::post('/task', function (Request $request) {
         //
+        // 驗證輸入
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+        return redirect('/');
+
+        // 建立該任務...
+        //新增任務存入DB的程式碼 (see next page)
     });
+
+});
 
 // 刪除任務
     Route::delete('/task/{task}', function (Task $task) {
