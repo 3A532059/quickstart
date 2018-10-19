14e20183893f5651a1e87dee28d09be7140db3b8
3A532059 <coco214034@gmail.com>
2018-10-19 14:48:30 +0800

    加入錯誤訊息檔案

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 陳姿婷
+ * Date: 2018/10/19
+ * Time: 下午 02:43
+ */
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
