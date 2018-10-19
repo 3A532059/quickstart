f941c390c2169260717abfc0651d2d75ac098a1b
3A532059 <coco214034@gmail.com>
2018-10-19 14:22:50 +0800

    建構三個路由

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,20 @@
 
 Route::get('/', function () {
     return view('welcome');
+    //顯示我們所有任務的清單
+    Route::get('/', function () {
+        return view('tasks');
+        //
+    });
+
+// 增加新的任務
+    Route::post('/task', function (Request $request) {
+        //
+    });
+
+// 刪除任務
+    Route::delete('/task/{task}', function (Task $task) {
+        //
+    });
+
 });
